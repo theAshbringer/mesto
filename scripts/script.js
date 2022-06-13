3b098f08caf13ae28e1b0700a0841868a3890d8f
theAsbringer <kristya1310@gmail.com>
2022-06-14 00:27:16 +0700

    ref: change names for variables

--- scripts/script.js
+++ scripts/script.js
@@ -7,44 +7,44 @@ let popup = document.querySelector(".popup");
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 // Форма и ее поля
-let editForm = document.querySelector(".popup__container");
-let nameFormField = document.querySelector(".popup__name");
-let descriptionFormField = document.querySelector(".popup__description");
+let formElement = document.querySelector(".popup__container");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__description");
 
 /** Инициализировать поля формы значениями из профиля */
 function initFormFields() {
-  nameFormField.value = profileName.innerText;
-  descriptionFormField.value = profileDescription.innerText;
+  nameInput.value = profileName.innerText;
+  jobInput.value = profileDescription.innerText;
 }
 
 /** Открыть форму */
-function openEditForm() {
+function openForm() {
   popup.classList.add("popup_opened");
   initFormFields();
 }
 
 /** Закрыть форму */
-function closeEditForm() {
+function closeForm() {
   popup.classList.remove("popup_opened");
 }
 
 /** Скопировать введенные данные в профиль */
 function updateProfile() {
   profileNameTextNode = profileName.childNodes[0];
-  profileNameTextNode.nodeValue = nameFormField.value;
-  profileDescription.innerText = descriptionFormField.value;
+  profileNameTextNode.nodeValue = nameInput.value;
+  profileDescription.innerText = jobInput.value;
 }
 
 /** Сохранить форму */
-function saveEditForm(evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   updateProfile();
-  closeEditForm();
+  closeForm();
 }
 
 // Событие "Редактировать профиль"
-editButton.addEventListener("click", openEditForm);
+editButton.addEventListener("click", openForm);
 // Событие "Закрыть попап"
-closeButton.addEventListener("click", closeEditForm);
+closeButton.addEventListener("click", closeForm);
 // Событие "Сохранить форму"
-editForm.addEventListener("submit", saveEditForm);
+formElement.addEventListener("submit", formSubmitHandler);
