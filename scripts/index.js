3ea0d97a33117949f0024992ec8b3e1d2543e855
theAsbringer <kristya1310@gmail.com>
2022-06-27 22:51:04 +0700

    feat: function for card rendering

--- scripts/index.js
+++ scripts/index.js
@@ -31,23 +31,7 @@ const addButton = document.querySelector('.add-btn');
 const editFormCloseButton = document.querySelector('.edit-profile .close-btn');
 const addFormCloseButton = document.querySelector('.add-card .close-btn');
 
-/** Отобразить начальные карточки при загрузке страницы */
-const initializeCards = () => {
   const cardTemplate = document.querySelector('#card-template').content;
-
-  initialCards.forEach((item) => {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = item.name;
-    cardElement.querySelector('.card__link').href = item.link;
-    cardElement.querySelector('.card__photo').src = item.link;
-    cardElement.querySelector('.card__photo').alt = item.alt;
-
-    const listItem = document.createElement('li');
-    listItem.append(cardElement);
-    const cardList = document.querySelector('.cards');
-    cardList.append(listItem);
-  });
-};
 
 // Попап с оверлеем
 const popup = document.querySelector('.popup');
@@ -64,6 +48,24 @@ const addCardForm = {
   formClass: '.add-card',
   topInput: '.popup__field_type_card-name',
   bottomInput: '.popup__field_type_card-link',
+};
+addFormElement = document.querySelector(addForm.formClass);
+
+/** Отрисовать карточку */
+const renderCard = (name, link) => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__photo').src = link;
+
+  const listItem = document.createElement('li');
+  listItem.append(cardElement);
+  const cardList = document.querySelector('.cards');
+  cardList.append(listItem);
+};
+
+/** Отобразить начальные карточки при загрузке страницы */
+const initializeCards = () => {
+  initialCards.forEach((item) => renderCard(item.name, item.link));
 };
 
 /** Инициализировать полt формы значениями из профиля */
