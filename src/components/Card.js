e6b7173214be40e0000e842fcc912692a4e35c8a
theAsbringer <kristya1310@gmail.com>
2022-08-29 21:01:19 +0700

    feat: display likes number

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ export default class Card {
     this._title = data.title;
     this._alt = data.title;
     this._image = data.image;
+    this._likes = data.likes;
     this._templateSelector = templateSelector;
+    this._template = document.querySelector(this._templateSelector).content;
+    this._likesNumber = this._template.querySelector('.card__likes-number');
     this._handleCardClick = handleCardClick;
     this._api = api;
   }
@@ -53,6 +56,7 @@ export default class Card {
     this._setEventListeners();
     this._cardImageElement.src = this._image;
     this._cardImageElement.alt = this._title;
+    this._likesNumber.textContent = this._likes;
     this._element.querySelector('.card__title').textContent = this._title;
 
     return this._element;
